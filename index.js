9bfc0a8e53bbf436af7e06a49107195b2490a5c9
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-18 19:36:04 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const addcats = [...cats];
+    addcats.push(name);
+    return addcats;
+}
+
+function prependCat(name){
+    const prepcat = [...cats];
+    prepcat.unshift(name);
+    return prepcat;
+}
+
+function removeLastCat(){
+    const remcat = [...cats];
+    remcat.pop();
+    return remcat;
+}
+
+function removeFirstCat(){
+    const remfircat = [...cats];
+    remfircat.shift();
+    return remfircat;
+}
